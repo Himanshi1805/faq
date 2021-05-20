d57eb6e40d4f638b517ed5631606cecd146da5cb
Himanshi1805 <82957564+Himanshi1805@users.noreply.github.com>
2021-05-20 20:25:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,90 @@
+
+   $(document).ready(function(){
+          $(".slide-toggle1").click(function(){
+              $(".box1").slideToggle();
+          });
+      });
+      $(document).ready(function(){
+             $(".slide-toggle2").click(function(){
+                 $(".box2").slideToggle();
+             });
+         });
+         $(document).ready(function(){
+                $(".slide-toggle3").click(function(){
+                    $(".box3").slideToggle();
+                });
+            });
+            $(document).ready(function(){
+                   $(".slide-toggle4").click(function(){
+                       $(".box4").slideToggle();
+                   });
+               });
+               $(document).ready(function(){
+                      $(".slide-toggle5").click(function(){
+                          $(".box5").slideToggle();
+                      });
+                  });
+
+
+
+
+
+
+
+
+
+
+
+
+// const arrow = document.querySelectorAll("li");
+// // const show = document.querySelectorAll('.ans');
+//
+// arrow.forEach(arrow => {
+//     arrow.addEventListener('click', ()=>{
+//         if(arrow.classList.contains('add')){
+//             arrow.classList.remove('add');
+//             arrow.classList.add('remove');*
+//             arrow.classList.remove('show');
+//             arrow.classList.add('hide');
+//         }
+//         else{
+//             arrow.classList.add('add');
+//             arrow.classList.remove('remove');
+//             arrow.classList.add('show');
+//             arrow.classList.remove('hide');
+//     }
+//     })
+// });
+
+
+// function keydown() {
+//    var ans = document.getElementByClassName("para p2");
+//   if (ans.style.display === "none") {
+//     ans.style.display = "block";
+//   } else {
+//     ans.style.display = "none";
+//   }
+// }
+//
+// // function 'keydown', function(event)
+//                                         {
+//              const key = event.key;
+//              switch (key) {
+//
+//                  case "js-arrow-up":
+//                   var ques = document.getElementByClass("para p1");
+//                      break;
+
+ // switch (*zxcvbnm,.1) {
+ //   case expression:
+ //
+ //     break;
+ //   default:
+
+ //                  case "js-arrow-down":
+//                      var ans = document.getElementByClass("para p2");
+//                      break;
+//
+//                      default: console.log(key);
+//              }
+// }
